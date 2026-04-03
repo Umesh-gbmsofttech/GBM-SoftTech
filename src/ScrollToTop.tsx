197a7767b784b3b4a3a417fd5c20899e3a842bb2
Shriram111 <shriramsgunjal@gmail.com>
2026-04-03 20:46:03 +0530

    ScrollToTop added

---
+++ src/ScrollToTop.tsx
@@ -0,0 +1,13 @@
+import { useEffect } from "react";
+import { useLocation } from "react-router-dom";
+
+export const ScrollToTop = () => {
+  const { pathname } = useLocation();
+
+  useEffect(() => {
+    // This moves the window to the top-left corner (0,0)
+    window.scrollTo(0, 0);
+  }, [pathname]); // This triggers every time the URL path changes
+
+  return null; // This component doesn't render anything visual
+};
